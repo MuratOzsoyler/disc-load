912ca61f76708c71b62882c1da1315f70da714b9
murat <murat.ozsoyler@gmail.com>
2020-11-22 03:52:27 +0300

    UI.Functions.hs is being rewritten

--- src/UI/Functions.hs
+++ src/UI/Functions.hs
@@ -1,58 +1,59 @@
 {-# LANGUAGE TypeApplications #-}
 module UI.Functions where
 
-import Data.Default.Class (Default (def))
-import Data.Int ( Int32 )
-import Data.Text as Text (Text, strip)
-import Data.Vector as Vector ((!), concat, cons, fromList, imap
-                             , modify, splitAt, toList, Vector
-                             )
-import qualified Data.Vector as Vector (map)
-import qualified Data.Vector.Mutable as MVector (modify)
+import Data.Default.Class (Default(def))
+import Data.Functor ((<&>))
+import Data.Text (strip, Text)
+import Data.Vector (toList, fromList)
+import GI.Gtk 
+        ( Align(AlignEnd, AlignFill)
+        , ApplicationWindow (..)
+        , Box(..)
+        , Button (..)
+        , Entry (..)
+        , get
+        , glibType
+        , Grid (..)
+        , Label (..)
+        , Orientation(OrientationHorizontal)
+        , PolicyType(PolicyTypeAutomatic)
+        , ScrolledWindow (..)
+        , Separator (..)
+        , unsafeCastTo
+        , Window (..)
+        )
+import GI.Gtk.Declarative 
+        ( Attribute((:=))
+        , bin
+        , BoxChild (..)
+        , container
+        , expand
+        , fill
+        , on
+        , onM
+        , widget
+        , Widget
+        )
+import GI.Gtk.Declarative.App.Simple (Transition (..), AppView, App (..), run)
+import GI.Gtk.Declarative.Container.Grid (topAttach, leftAttach, width, GridChild(..))
+import UI.Types (ItemInfo (..), InputEvent (..), InputState (..))
+import DiscHandling.Utils (showText)
 
-import GI.Gtk.Declarative (fill
-                          , expand, widget, Attribute((:=)), bin, container, on
-                          , onM
-                          )
-import GI.Gtk.Declarative.Container.Box (BoxChild(BoxChild))
-import GI.Gtk.Declarative.Container.Grid (width, leftAttach, topAttach
-                                         , GridChild (..)
-                                         )
-import GI.Gtk.Declarative.App.Simple (App(..), AppView, run, Transition(..))
-import GI.Gtk (Label(Label), Box(Box), Orientation(OrientationHorizontal)
-              , Separator(Separator), Align(AlignFill, AlignCenter, AlignEnd), Entry(Entry)
-              , Grid(Grid), ApplicationWindow(ApplicationWindow)
-              , get, Button (Button), Window (Window), glibType
-              , unsafeCastTo, ScrolledWindow (ScrolledWindow), PolicyType(..)
-              )
+runInput :: InputState -> IO InputState
+runInput = run . mkApp
 
-import DiscHandling.Utils (showText,  getSanitize )
-import UI.Types
-    ( InputEvent(..),
-      InputResult(InputResultRipDisc),
-      InputState(..),
-      ItemInfo(..) )
+mkApp :: InputState -> App ApplicationWindow InputState InputEvent
+mkApp state = App
+    { view = inputView
+    , update = inputUpdate
+    , inputs = mempty
+    , initialState = state 
+    }
 
-expandFillTable :: Vector (Bool, Bool, Bool, Align)
-expandFillTable = 
-    [ (True,  True,  True,  AlignFill)
-    , (True,  True,  True,  AlignCenter)
-    , (True,  True,  False, AlignFill)
-    , (True,  True,  False, AlignCenter)
-    , (True,  False, True,  AlignFill)
-    , (True,  False, True,  AlignCenter)
-    , (True,  False, False, AlignFill)
-    , (True,  False, False, AlignCenter)
-    , (False, True,  True,  AlignFill)
-    , (False, True,  True,  AlignCenter)
-    , (False, True,  False, AlignFill)
-    , (False, True,  False, AlignCenter)
-    , (False, False, True,  AlignFill)
-    , (False, False, True,  AlignCenter)
-    , (False, False, False, AlignFill)
-    , (False, False, False, AlignCenter)
-    ]
-
+inputUpdate :: InputState -> InputEvent -> Transition InputState InputEvent
+inputUpdate InputState {..} = error "not implemented"
+ 
+    
 inputView :: InputState -> AppView ApplicationWindow InputEvent
 inputView InputState {..} = bin
     ApplicationWindow
@@ -67,84 +68,68 @@ inputView InputState {..} = bin
     $ container
         Grid
         [#hexpand := True, #rowSpacing := 2, #columnSpacing := 2, #margin := 4]
-        $ Vector.concat 
-            [ fromList
-                [ GridChild def {width = 2} $ widget Label [#label := "Title"]
-                , GridChild def {leftAttach = 2} $ widget Label [#label := "From"]
-                ] 
-            , let (i, t) = Vector.splitAt 1 $ itemView 1 0 "album" albumInfo
-                  i' = Vector.map (\GridChild {..} -> GridChild properties {width = 2} child) i
-                  t' = Vector.map (\GridChild {..} -> GridChild properties {leftAttach = 2} child) t
-              in i' <> t'
-            , fromList 
-                [GridChild 
-                    def {topAttach = 2, leftAttach = 0, width = 3} 
-                    $ container Box [#hexpand := True, #orientation := OrientationHorizontal, #spacing := 2]
-                        [ widget Label [#label := "Tracks"]
-                        , BoxChild def {fill = boxChildFill, expand = boxChildExpand}
-                            $ widget Separator 
-                                [ #hexpand := sepExpand
-                                , #halign := sepFill
-                                , #orientation := OrientationHorizontal
-                                ] 
-                        ]
-                ]
-            , Vector.concat 
-                $ zipWith trackView [1..]
-                $ zip3 [3..] (repeat "track") (toList trackInfos)
-            , fromList 
-                [ GridChild 
-                    def {topAttach = fromIntegral $ length trackInfos + 3, leftAttach = 0, width = 2} 
-                    $ container Box [#hexpand := True, #orientation := OrientationHorizontal, #spacing := 2]
-                        [ widget Button 
-                            [ #label := "Rip Disc"
-                            , onM #clicked $ quitGUI OK
-                            ]
-                        , widget Button 
-                            [ #label := "Cancel"
-                            , onM #clicked $ quitGUI Cancel
-                            ]
-                        ]
-                ]
-            ]
-  where
-    idx = expandFillIdx
-    (boxChildExpand, boxChildFill, sepExpand, sepFill) = expandFillTable ! idx
-    trackView :: Int -> (Int32, Text, ItemInfo) -> Vector (GridChild InputEvent)
-    trackView trackIdx (row, itemLabel, itemInfo) =
-        Vector.imap 
-            (\idx GridChild {..} -> GridChild (properties {leftAttach = fromIntegral idx}) child)
-            $ GridChild 
-                def {topAttach = row}
-                (widget Label 
-                    [ #label := showText trackIdx
-                    , #halign := AlignEnd
-                    ])
-            `cons` 
-                itemView row 1 itemLabel itemInfo
-    itemView :: Int32 -> Int32 -> Text -> ItemInfo -> Vector (GridChild InputEvent)
-    itemView row startCol itemLabel ItemInfo {..} = fromList $ map
-        (\(col, typ, entry) -> gridChild 
-            (props row col) 
-            (entryWidget typ itemLabel row col entry)
+    $ fromList 
+        $ (concat :: [[GridChild InputEvent]] -> [GridChild InputEvent]) 
+        $ (\(r, gs) -> map (\GridChild {..} -> GridChild (propTopAtch r properties) child) gs)
+        <$> zip [0..]
+            ([ headers
+            , albumSep
+            , albumEntryRow
+            , tracksSep
+            ] 
+            ++ trackInfoRows
+            ++ buttonsRow
             )
-        (zip3 [startCol ..] ["title", "from"] [title, from])
-      where
-        props row col p = p { leftAttach = col, topAttach = row }
-    gridChild propf widgetf = GridChild (propf def) widgetf
-    entryWidget itemType itemLabel row col value = widget Entry 
-        [ #hexpand := True
-        , #halign := AlignFill
-        , #placeholderText := ("Enter \"" <> itemType <> "\" value for " <> itemLabel)
-        , #text := value
-        , onM #changed $ \entry -> do
-            newVal <- get entry #text
-            let newValue = strip newVal
-                result = if value == newValue 
-                    then NotChanged
-                    else Changed row col newValue
-            return result
+  where
+    headers :: [GridChild InputEvent]
+    headers = 
+        [ gridChild (\p -> p {width = 2}) $ labelWgt "Title"
+        , gridChild (\p -> p {leftAttach = 2}) $ labelWgt "From"
         ]
+    albumSep :: [GridChild InputEvent]
+    albumSep = genericSep "Album"
+    albumEntryRow :: [GridChild InputEvent]
+    albumEntryRow = 
+        let ItemInfo {..} = albumInfo
+        in  [ gridChild propWidth2 
+                (entryWidget (mkPlaceHolder "title" "album") title 
+                    <&> entEvt2InpEvt (TitleChanged (-1)) 
+                    )
+            , gridChild propLeftAtch2
+                (entryWidget (mkPlaceHolder "from" "album") from
+                    <&> entEvt2InpEvt (FromChanged (-1))
+                    )
+            ]
+    tracksSep = genericSep "Tracks"
+    trackInfoRows :: [[GridChild InputEvent]]
+    trackInfoRows = zipWith 
+        (\i ItemInfo {..} -> 
+            [ gridChild id $ widget Label [#label := showText (i + 1), #halign := AlignEnd]
+            , gridChild propLeftAtch1
+                (entryWidget (mkPlaceHolder "title" "track") title
+                    <&> entEvt2InpEvt (TitleChanged i)
+                    ) 
+            , gridChild propLeftAtch2 
+                (entryWidget (mkPlaceHolder "from" "track") from
+                    <&> entEvt2InpEvt (FromChanged i)
+                    )
+            ])
+        [0..]
+        $ toList trackInfos
+    buttonsRow :: [[GridChild InputEvent]]
+    buttonsRow =
+        [[ gridChild propWidth2 
+            $ container Box [#hexpand := True, #orientation := OrientationHorizontal, #spacing := 2]
+                [ widget Button 
+                    [ #label := "Rip Disc"
+                    , onM #clicked $ quitGUI OK
+                    ]
+                , widget Button 
+                    [ #label := "Cancel"
+                    , onM #clicked $ quitGUI Cancel
+                    ]
+                ]
+        ]]
     quitGUI :: InputEvent -> Button -> IO InputEvent
     quitGUI evt button = do
         typ <- glibType @ApplicationWindow
@@ -155,53 +140,39 @@ inputView InputState {..} = bin
                 win <- unsafeCastTo Window wgt 
                 #close win
         return evt
+    genericSep :: Text -> [GridChild InputEvent]
+    genericSep title =
+        [ gridChild (\p -> p {width = 3}) 
+            $ container 
+                Box 
+                [#hexpand := True, #orientation := OrientationHorizontal, #spacing := 2]
+                [ widget Label [#label := title]
+                , BoxChild def {fill = True, expand = True}
+                    $ widget Separator 
+                        [ #hexpand := False
+                        , #halign := AlignFill
+                        , #orientation := OrientationHorizontal
+                        ] 
+                ]
+        ]
+    entEvt2InpEvt inpEvtCons (EntryChanged val) = inpEvtCons val
+    mkPlaceHolder typ label = "Enter \"" <> typ <> "\" value for " <> label
+    labelWgt lbl = widget Label [#label := lbl]
+    gridChild propf = GridChild (propf def)
+    propWidth2 p = p { width = 2 } 
+    propLeftAtch1 p = p { leftAttach = 1 } 
+    propLeftAtch2 p = p { leftAttach = 2 } 
+    propTopAtch r p = p { topAttach = r } 
 
-inputUpdate :: InputState -> InputState -> InputEvent -> Transition InputState InputEvent
-inputUpdate reverted state@InputState {..} = 
-    let sanitized = sanitize state
-    in \case
-        Closed -> Exit
-        Escaped -> Transition reverted (return $ Just Closed) 
-        Clicked -> Transition (sanitized { expandFillIdx = (expandFillIdx + 1) `mod` 16 }) (return Nothing)
-        OK -> Transition (sanitized { inputResult = InputResultRipDisc }) (return Nothing)
-        Cancel -> Transition reverted (return Nothing)
-        NotChanged -> Transition state (return Nothing)
-        Changed row col value -> 
-            let row' = fromIntegral row
-            in  Transition 
-                (sanitize $ case row' of
-                    1 -> state { albumInfo = modifyItemInfo albumInfo }
-                    _ | row' >= 3 && row' < length trackInfos + 3 ->
-                        let idx = row' - 3
-                        in state
-                            { trackInfos = Vector.modify 
-                                (\mvec -> MVector.modify
-                                    mvec
-                                    modifyItemInfo
-                                    idx
-                                )
-                                trackInfos
-                            }
-                      | otherwise -> error $ "undefined row value = " <> show row'
-                )
-                (return Nothing)
-          where
-            modifyItemInfo info = case col of
-                0 -> info { title = value }
-                1 -> info { from = value }
+data EntryChangeEvent = EntryChanged Text
 
-runInput :: InputState -> IO InputState
-runInput state = run . mkInput state =<< getSanitize
-
-mkInput 
-    :: InputState 
-    -> (InputState -> InputState) 
-    -> App ApplicationWindow InputState InputEvent
-mkInput params sanitize = 
-    let params' = params { sanitize = sanitize }
-    in App
-        { view = inputView
-        , update = inputUpdate params'
-        , inputs = mempty
-        , initialState = params'
-        }
+entryWidget :: Text -> Text -> Widget EntryChangeEvent
+entryWidget plcHolder value = widget Entry 
+    [ #hexpand := True
+    , #halign := AlignFill
+    , #placeholderText := plcHolder
+    , #text := value
+    , onM #changed $ \entry -> do
+        newVal <- get entry #text
+        return $ EntryChanged $ strip newVal
+    ]
